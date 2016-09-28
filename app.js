5944cc3879d569414b295b6bfc75d98a712fd4dd
kumar-nielsen <sarthak.kumar@nielsen.com>
2016-09-28 16:40:49 -0500

    Updated to receive correct PAGE_ACCESS_TOKEN process variable

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
   config.get('validationToken');
 
 // Generate a page access token for your page from the App Dashboard
-const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+const PAGE_ACCESS_TOKEN = (process.env.PAGE_ACCESS_TOKEN) ?
   (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
   config.get('pageAccessToken');
 
